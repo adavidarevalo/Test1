6c6b0c8bd1fc460a7078e5af99f629a70b65f24d
David Arevalo <davidarevaloc20@gmail.com>
2021-11-28 18:18:43 -0500

    Resolve the test

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "31cc2b632084a6ea"; }
+/******/ 		__webpack_require__.h = function() { return "dc73559a8d230ac4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
